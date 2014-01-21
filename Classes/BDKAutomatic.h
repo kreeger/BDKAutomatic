9a84dff6bc189867a59dd53186447fc08f81750f
Ben Kreeger <ben@kree.gr>
2014-01-20 18:01:15 -0600

    Adding some stub files.

---
+++ Classes/BDKAutomatic.h
@@ -0,0 +1,14 @@
+//
+//  BDKAutomatic.h
+//  BDKAutomatic
+//
+//  Created by Ben Kreeger on 1/20/14.
+//  Copyright (c) 2014 Ben Kreeger. All rights reserved.
+//
+
+@import Foundation;
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+
+@interface BDKAutomatic : AFHTTPRequestOperationManager
+
+@end
---
+++ Classes/BDKAutomatic.m
@@ -0,0 +1,15 @@
+//
+//  BDKAutomatic.m
+//  BDKAutomatic
+//
+//  Created by Ben Kreeger on 1/20/14.
+//  Copyright (c) 2014 Ben Kreeger. All rights reserved.
+//
+
+#import "BDKAutomatic.h"
+
+@implementation BDKAutomatic
+
+
+
+@end
