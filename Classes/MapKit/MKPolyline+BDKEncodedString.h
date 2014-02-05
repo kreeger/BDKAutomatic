78e893166c5471b7a73139249c6b30fbc142736f
Ben Kreeger <ben@kree.gr>
2014-02-05 08:20:23 -0600

    Fixing Travis YAML file.

--- Classes/MapKit/MKPolyline+BDKEncodedString.h
+++ Classes/MapKit/MKPolyline+BDKEncodedString.h
@@ -2,6 +2,9 @@
 
 @interface MKPolyline (BDKEncodedString)
 
+/**
+ Praise be to http://stackoverflow.com/a/9219856/194869
+ */
 + (MKPolyline *)polylineWithEncodedString:(NSString *)encodedString;
 
 @end
