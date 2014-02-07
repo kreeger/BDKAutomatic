0b40581781e187a7d8951916f25304a8ec59d158
Ben Kreeger <ben@kree.gr>
2014-02-06 21:19:05 -0600

    Well hello there, map view.

--- Project/Classes/BDKMapViewController.m
+++ Project/Classes/BDKMapViewController.m
@@ -13,7 +13,10 @@
 #import <BDKAutomatic/BDKAutomaticTrip.h>
 #import <BDKAutomatic/MKPolyline+BDKEncodedString.h>
 
-@interface BDKMapViewController ()
+@interface BDKMapViewController () <MKMapViewDelegate>
+
+@property (strong, nonatomic) MKMapView *mapView;
+@property (strong, nonatomic) MKPolyline *mapPath;
 
 @end
 
@@ -22,14 +25,53 @@
 - (void)loadView {
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.mapView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.mapView addOverlay:self.mapPath level:MKOverlayLevelAboveRoads];
+    NSLog(@"mapView=%p, overlay count = %d", self.mapView, self.mapView.overlays.count);
+    [self.mapView setVisibleMapRect:[self.mapPath boundingMapRect]
+                        edgePadding:UIEdgeInsetsMake(100, 100, 100, 100)
+                           animated:YES];
+}
+
+- (void)dealloc {
+    self.mapView.delegate = nil;
 }
 
 #pragma mark - Properties
 
+- (MKMapView *)mapView {
+    if (_mapView) return _mapView;
+    _mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
+    _mapView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    _mapView.delegate = self;
+    return _mapView;
+}
+
 - (void)setTrip:(BDKAutomaticTrip *)trip {
     self.title = [NSString stringWithFormat:@"%@", trip.identifier];
-    MKPolyline *line = [MKPolyline polylineWithEncodedString:trip.path];
-    NSLog(@"Polyline is %@", line);
+    self.mapPath = [MKPolyline polylineWithEncodedString:trip.path];
+}
+
+#pragma mark - MKMapViewDelegate
+
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    if ([overlay isKindOfClass:[MKPolyline class]]) {
+        MKPolylineRenderer *renderer = [[MKPolylineRenderer alloc] initWithPolyline:overlay];
+        renderer.lineWidth = 3.0;
+        renderer.strokeColor = [UIColor blueColor];
+        return renderer;
+    } else {
+        return nil;
+    }
 }
 
 @end
